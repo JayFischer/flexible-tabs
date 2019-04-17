cdb325320114846137b1232605b84f2f238a8971
Conor Fischer <conor.fischer@gmail.com>
2019-04-17 01:34:14 -0500

    feat: Initialize Project
    Initialize Chrome-Extension project Flexible-Tabs. Add simple implementation of Right, Left, Other tab close. Add placeholder methods for Domain and Active tab close.

---
+++ src/bg/background.js
@@ -0,0 +1,79 @@
+chrome.runtime.onInstalled.addListener(function() {
+  const flexTab = { title: "Flexible Tabs", id: "flexTabs" };
+  const flexTabsChildren = [
+    { title: "Close Tabs to the Right", id: "right" },
+    { title: "Close Tabs to the Left", id: "left" },
+    { title: "Close Tabs like Domain", id: "domain" },
+    { title: "Close Other Tabs", id: "other" },
+    { title: "Close Tab", id: "tab" }
+  ];
+
+  chrome.contextMenus.create(flexTab);
+  flexTabsChildren.forEach(tab =>
+    chrome.contextMenus.create({ parentId: flexTab.id, ...tab })
+  );
+});
+
+const getActiveWindowTabs = callback => {
+  chrome.windows.getCurrent({}, function(window) {
+    chrome.tabs.query({ windowId: window.id }, function(tabs) {
+      const activeTab = tabs.find(tab => tab.active === true);
+      callback(tabs, activeTab);
+    });
+  });
+};
+
+const closeRight = () => {
+  const tabs = getActiveWindowTabs(function(tabs, activeTab) {
+    const tabsToRemove = tabs.slice(activeTab.index + 1).map(tab => tab.id);
+    chrome.tabs.remove(tabsToRemove);
+  });
+};
+
+const closeLeft = () => {
+  const tabs = getActiveWindowTabs(function(tabs, activeTab) {
+    const tabsToRemove = tabs.slice(0, activeTab.index).map(tab => tab.id);
+    chrome.tabs.remove(tabsToRemove);
+  });
+};
+
+const closeOther = () => {
+  const tabs = getActiveWindowTabs(function(tabs, activeTab) {
+    const tabsToRemove = tabs
+      .filter(tab => tab.id !== activeTab.id)
+      .map(tab => tab.id);
+    chrome.tabs.remove(tabsToRemove);
+  });
+};
+
+// TODO : Implement close based on simular URL domains
+const closeDomain = () => {
+  return null;
+};
+
+// TODO : Implement close on active tab
+const closeTab = () => {
+  return null;
+};
+
+chrome.contextMenus.onClicked.addListener(function(item, tab) {
+  switch (item.menuItemId) {
+    case "left":
+      closeLeft();
+      break;
+    case "right":
+      closeRight();
+      break;
+    case "other":
+      closeOther();
+      break;
+    case "tab":
+      closeTab();
+      break;
+    case "domain":
+      closeDomain();
+      break;
+    default:
+      return null;
+  }
+});
